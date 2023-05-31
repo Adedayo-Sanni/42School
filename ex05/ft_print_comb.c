02594aed61e144c6938dc60ef241b686e332f595
Adedayo Sanni <asanni@student.42sp.org.br>
2023-05-31 17:59:23 -0300

    adicionando todos os exercícios

---
+++ ex05/ft_print_comb.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 10:19:45 by asanni            #+#    #+#             */
+/*   Updated: 2023/05/31 17:57:27 by asanni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_print_comb(void)
+void ft_combination(int centena, int dezena, int unidade)
+{
+	int centena;
+	int dezena;
+	int unidade;
+
+	centena = 0;
+	dezena = 1;
+	unidade = 2;
+}
