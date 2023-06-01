7e43066d74208c7822256fb5b08d0006577c148c
Adedayo Sanni <asanni@student.42sp.org.br>
2023-06-01 10:59:46 -0300

    adicionando todas as mudanças

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,20 +6,41 @@
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 10:19:45 by asanni            #+#    #+#             */
-/*   Updated: 2023/05/31 17:57:27 by asanni           ###   ########.fr       */
+/*   Updated: 2023/05/31 19:03:31 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_comb(void)
-void ft_combination(int centena, int dezena, int unidade)
+void	ft_combination(char c, char d, char u)
 {
-	int centena;
-	int dezena;
-	int unidade;
+	write(1, &c, 1);
+	write(1, &d, 1);
+	write(1, &u, 1);
+	if (c != '7' || d != '8' || u != '9')
+		write(1, ", ", 2);
+}
 
-	centena = 0;
-	dezena = 1;
-	unidade = 2;
+void	ft_print_comb(void)
+{
+	char	centena;
+	char	dezena;
+	char	unidade;
+
+	centena = '0';
+	while (centena <= '7')
+	{
+		dezena = centena + 1;
+		while (dezena <= '8')
+		{
+			unidade = dezena + 1;
+			while (unidade <= '9')
+			{
+				ft_combination(centena, dezena, unidade);
+				unidade++;
+			}
+			dezena++;
+		}
+		centena++;
+	}
 }
