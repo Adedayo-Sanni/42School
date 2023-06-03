d3ae27c9a41f1c3eb3a23bd1b52f19121285c08e
Adedayo Sanni <asanni@student.42sp.org.br>
2023-06-03 18:28:45 -0300

    enviando todos os excs

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -5,30 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/01 13:45:39 by asanni            #+#    #+#             */
-/*   Updated: 2023/06/02 20:29:52 by asanni           ###   ########.fr       */
+/*   Created: 2023/06/03 12:22:57 by asanni            #+#    #+#             */
+/*   Updated: 2023/06/03 17:46:59 by asanni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_col(int colprinc, int max)
+void	ft_print_col(int colprinc)
 {
 	char	um;
 	char	dois;
-	
+
 	if (colprinc <= 9)
 	{
 		um = colprinc + 48;
 		write(1, "0", 1);
 		write(1, &um, 1);
 	}
-	else if(colprinc <= max)
+	else if (colprinc <= 99)
 	{
-		um = colprinc + 48;
 		dois = colprinc + 48;
 		um = (colprinc % 10) + 48;
-		dois = (colprinc /10) + 48;
+		dois = (colprinc / 10) + 48;
 		write(1, &dois, 1);
 		write(1, &um, 1);
 	}
@@ -45,7 +44,16 @@ void	ft_print_comb2(void)
 		col2 = col1 + 1;
 		while (col2 <= 99)
 		{
-			ft_print_col(col1, col2);
+			if (col1 < col2)
+			{
+				ft_print_col(col1);
+				write(1, " ", 1);
+				ft_print_col(col2);
+			}
+			if (col1 != 98)
+			{
+				write(1, ", ", 2);
+			}
 			col2++;
 		}
 		col1++;
